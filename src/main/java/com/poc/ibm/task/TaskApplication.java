5f01d986685643d403ee673b59a07e357dd75805
Alex Soares da Silva <alex.silva.contratada@lasa.com.br>
2019-08-08 20:43:07 -0300

    commit

--- src/main/java/com/poc/ibm/task/TaskApplication.java
+++ src/main/java/com/poc/ibm/task/TaskApplication.java
@@ -2,12 +2,22 @@ package com.poc.ibm.task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@Controller
 public class TaskApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String index(Model model) {
+		return "index";
 	}
 
 }
